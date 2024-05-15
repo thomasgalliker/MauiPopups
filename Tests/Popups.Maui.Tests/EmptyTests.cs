9656220b46a566de60e62c555275ff69d0f3c1ca
Thomas Galliker <thomas@bluewin.ch>
2024-05-15 16:02:00 +0200

    Add empty tests

---
+++ Tests/Popups.Maui.Tests/EmptyTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Popups.Maui.Tests
+{
+    public class EmptyTests
+    {
+        [Fact]
+        public void ShouldDoSomething_WhenCondition_ExpectedResult()
+        {
+            // Arrange
+
+            // Act
+
+
+            // Assert
+
+        }
+    }
+}
