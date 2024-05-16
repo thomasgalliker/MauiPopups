bc330f0b986226bbff1904ce62bb502040497056
Thomas Galliker <thomas@bluewin.ch>
2024-05-16 08:37:21 +0200

    Add xmlns definition sample

--- Popups.Maui/Properties/AssemblyConfig.cs
+++ Popups.Maui/Properties/AssemblyConfig.cs
@@ -3,3 +3,5 @@
 [assembly: InternalsVisibleTo("Popups.Maui.Tests")]
 [assembly: InternalsVisibleTo("MauiSampleApp")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: XmlnsDefinition("http://schemas.superdev.ch/dotnet/2021/popups.maui", "Popups.Maui")]
